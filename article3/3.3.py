43078150ecb1d1cc3f224990c3196b87338a9a8c
Zhuoooo <16620154495@163.com>
2023-02-28 21:37:38 +0800

    3.3 节

---
+++ article3/3.3.py
@@ -0,0 +1,23 @@
+where = ['shanghai', 'xinjiang', 'xizang', 'yunnan', 'guangxi']
+print(where)
+
+print(sorted(where))
+print(where)
+
+print(sorted(where).reverse())
+
+print(where)
+
+where.reverse()
+
+print(where)
+
+where.reverse()
+
+print(where)
+
+where.sort()
+print(where)
+
+where.sort()
+print(where)
